31e3096a11859af58942d62cd123b8c2b6eea6bc
Bartek <aqfgit@gmail.com>
2018-06-08 20:38:15 +0200

    Template properly renders result from scraper

--- busstops/views.py
+++ busstops/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from utils import scraper
 
 @login_required
 def index_view(request):
-    return render(request, 'busstops/bus-search.html')
+    data = scraper.run()
+    return render(request, 'busstops/bus-search.html', {'data': data})
--- utils/scraper.py
+++ utils/scraper.py
@@ -2,21 +2,25 @@ import bs4 as bs
 import requests
 
 #python -m run 1.py
+def run():
+    url = 'http://pksbielsko.stop.net.pl/rjaWyszukiwarkaPolaczen.php?pom=2&data_pol=2018-06-06&sz_el_1=RUDZICA%2C+1&sz_el_2=BIELSKO-BIAŁA%2C+D.A.&z_miejsca=7221150&do_miejsca=7220108&submit=Szukaj+po%C5%82%C4%85czenia'
 
-url = 'http://pksbielsko.stop.net.pl/rjaWyszukiwarkaPolaczen.php?pom=2&data_pol=2018-06-06&sz_el_1=RUDZICA%2C+1&sz_el_2=BIELSKO-BIA%C5%81A%2C+D.A.&z_miejsca=7221150&do_miejsca=7220108&submit=Szukaj+po%C5%82%C4%85czenia'
+    source = requests.get(url)
+    source.encoding = 'utf-8'
+    soup = bs.BeautifulSoup(source.text, 'html.parser')
 
-source = requests.get(url).text
-soup = bs.BeautifulSoup(source, 'html.parser')
+    table = soup.find('table')
+    table_rows = table.find_all('tr')
 
-table = soup.find('table')
-table_rows = table.find_all('tr')
+    rows = []
 
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text for i in td]
+        rows.append(row)
 
-    
+    with open('somefile.txt', 'w') as the_file:
+        the_file.write(rows[1][5])
 
-
-
+    return rows
+   
